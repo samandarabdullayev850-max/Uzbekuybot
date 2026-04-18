6bf3752e2e694407c0d651850d93a3f693554223
samandarabdullayev850-max <samandarabdullayev850@gmail.com>
2026-04-18 17:24:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -374,6 +374,4 @@ async def main():
     await app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
-    import nest_asyncio
-    nest_asyncio.apply()
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
