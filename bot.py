1bbb727ad37dacf2926adc3f36fe2d21de8506f7
samandarabdullayev850-max <samandarabdullayev850@gmail.com>
2026-04-18 19:14:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -363,4 +363,6 @@ async def main():
     await app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
